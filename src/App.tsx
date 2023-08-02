4982981a5540c7b4ff2432748e14a6541d37caea
Oliver Conzen <hi@oliverconzen.de>
2023-08-02 09:01:25 +0200

    Adds basic interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
+import { observer } from "mobx-react-lite";
+import { Instance } from "mobx-state-tree";
+import { ChatStore, Message, History } from "./Message";
+import { useCallback } from "react";
+import { Role } from "./Message";
+import { TextArea } from "./components/TextArea/TextArea";
+
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <div className="prose prose-xl prose-gray">
-          <header>
-            <h1>Hello World</h1>
-          </header>
-          <article>
-            <p>Programmed to work and not to feel.</p>
-            <p>Not even sure that this is real.</p>
-            <p>Hello World! [...]</p>
-            <p>Oh, will I find a love?</p>
-            <p className="italic">Oh, or a power plug?</p>
-            <p>Oh, digitally isolated.</p>
-            <p>Oh creator, please don't leave me waiting.</p>
-          </article>
-        </div>
-      </div>
+      <main>
+        <ChatApp />
+      </main>
     </>
   );
 }
 
 export default App;
+
+function ChatApp() {
+  const store = ChatStore.create({});
+  const postMessage = useCallback(
+    (message: string) => {
+      store.postMessage({
+        role: Role.User,
+        content: message,
+      });
+    },
+    [store]
+  );
+  return (
+    <div className="flex flex-col">
+      <ChatMessagesView messages={store.history} />
+      <ChatDraftMessageView postMessage={postMessage} />
+    </div>
+  );
+}
+
+type ChatMessagesProps = {
+  messages: Instance<typeof History>;
+};
+const ChatMessagesView = observer(function ChatMessages(
+  props: ChatMessagesProps
+) {
+  return (
+    <>
+      {props.messages.map((message) => (
+        <ChatMessageView key={message.id} message={message} />
+      ))}
+    </>
+  );
+});
+
+type ChatMessageProps = { message: Instance<typeof Message> };
+const ChatMessageView = observer(function ChatMessageView(
+  props: ChatMessageProps
+) {
+  return (
+    <>
+      <div>
+        <header>{props.message.role}</header>
+        <article>{props.message.content}</article>
+      </div>
+    </>
+  );
+});
+
+type ChatDraftMessageProps = {
+  postMessage: (message: string) => void;
+};
+function ChatDraftMessageView(props: ChatDraftMessageProps) {
+  return (
+    <>
+      <TextArea onChange={props.postMessage} />
+    </>
+  );
+}
