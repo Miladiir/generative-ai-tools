4efcf8015e1d41ace3df29c0e09a13dfeb19bfc1
Oliver Conzen <hi@oliverconzen.de>
2023-08-02 09:01:14 +0200

    Adds some basic api call.

--- src/Message.ts
+++ src/Message.ts
@@ -1,6 +1,7 @@
-import { types } from "mobx-state-tree";
+import { Instance, SnapshotIn, flow, types } from "mobx-state-tree";
+import OpenAI from "openai";
 
-enum Role {
+export enum Role {
   System = "system",
   User = "user",
   Assistant = "assistant",
@@ -8,8 +9,57 @@ enum Role {
 }
 
 export const Message = types.model("Message", {
-    role: types.enumeration(Object.values(Role)),
-    content: types.string,
+  role: types.enumeration(Object.values(Role)),
+  content: types.string,
+  id: types.optional(types.string, () => crypto.randomUUID()),
 });
 
 export const History = types.array(Message);
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_APIKEY,
+});
+async function* getCompletion(
+  messages: Instance<typeof History>
+): Promise<SnapshotIn<typeof Message>> {
+  const messagesOut = messages.map((message) => ({
+    role: message.role,
+    content: message.content,
+  }));
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: messagesOut,
+  });
+  const responseMessage = response.choices[0].message;
+  yield {
+    role: responseMessage.role as Role,
+    content: responseMessage.content ?? "",
+  };
+}
+
+export const ChatStore = types
+  .model("ChatStore", {
+    history: types.optional(History, []),
+    loading: types.optional(types.boolean, false),
+    error: types.optional(types.union(types.string, types.null), null),
+  })
+  .actions((self) => ({
+    postMessage: flow(function* postMessage(
+      message: Instance<typeof Message> | SnapshotIn<typeof Message>
+    ) {
+      self.error = null;
+      self.loading = true;
+      self.history.push(message);
+      if (message.role === "user") {
+        try {
+          const response = yield getCompletion(self.history);
+          self.history.push(response);
+          self.error = null;
+          self.loading = false;
+        } catch (e) {
+          self.error = String(e);
+          self.loading = false;
+        }
+      }
+    }),
+  }));
