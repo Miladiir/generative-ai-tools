d15b0fa853f91f8483cab44bafe2481bcdb9a1f8
Oliver Conzen <hi@oliverconzen.de>
2023-08-06 22:58:35 +0200

    Remove mock from build.

--- src/data/RootStore.ts
+++ src/data/RootStore.ts
@@ -1,7 +1,6 @@
-import { getSnapshot, types } from "mobx-state-tree";
+import { types } from "mobx-state-tree";
 import { ChatStore } from "./ChatStore";
-import { chatStoreMock } from "./ChatStore.mock";
 
 export const RootStore = types.model("RootStore", {
-  chatStore: types.optional(ChatStore, () => getSnapshot(chatStoreMock),//ChatStore.create()),
-  )});
+  chatStore: types.optional(ChatStore, () => ChatStore.create()),
+});
