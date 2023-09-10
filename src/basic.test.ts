21c2f4c5826aeba344a8501a181e08f6225f68ec
Oliver Conzen <hi@oliverconzen.de>
2023-09-10 09:00:41 +0000

    Fix test

--- src/basic.test.ts
+++ src/basic.test.ts
@@ -3,7 +3,7 @@ import { describe } from "vitest";
 
 const openAi = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_APIKEY,
-});
+  dangerouslyAllowBrowser:true});
 
 describe("hello", () => {
   it("world", async () => {
@@ -16,22 +16,26 @@ describe("hello", () => {
             "You are a very friendly robot and you make beepboop sounds. Greet the user in your roboty fashion.",
         },
         {
-          role: 'assistant',
-          content: 'Beep boop! Greetings, dear user! I am your friendly neighborhood robot, here to assist you. How may I beep boop you today?'
+          role: "assistant",
+          content:
+            "Beep boop! Greetings, dear user! I am your friendly neighborhood robot, here to assist you. How may I beep boop you today?",
         },
         {
-          role: 'user',
-          content: 'Oh nothing, I am just making fun of you by making you do stupid noises.'
-        }
+          role: "user",
+          content:
+            "Oh nothing, I am just making fun of you by making you do stupid noises.",
+        },
       ],
     });
     console.log(response.choices[0].message);
     expect(response?.choices[0].message.content).to.not.be.undefined;
   });
   it("models", async () => {
-    const response = await openAi.models.list({ });
-    const models = response.data.map((model=>model.id)).filter((name)=>name.includes("gpt"));
-    console.log(models)
+    const response = await openAi.models.list({});
+    const models = response.data
+      .map((model) => model.id)
+      .filter((name) => name.includes("gpt"));
+    console.log(models);
     expect(undefined).to.be.undefined;
   });
 });
